c25493f241813f9d2e0619ce52fb863ef084ad55
tohami <tohami.tohami@orchtech.com>
2018-07-22 10:17:28 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+// @flow
 import React , { Component } from 'react' ;
 import { View , Text } from 'react-native' ;
-import Provider from 'react-redux' ;
+import {Provider} from 'react-redux' ;
 import { createStore } from 'redux' ;
+import firebase from 'firebase' ;
+import reducers from './reducers';
 
-class App extends Component {
+class App extends Component  {
+
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCgzmdN6QMLkrbNYvhcuBojZUNgD1VCQkQ',
+            authDomain: 'manager-eba8c.firebaseapp.com',
+            databaseURL: 'https://manager-eba8c.firebaseio.com',
+            projectId: 'manager-eba8c',
+            storageBucket: 'manager-eba8c.appspot.com',
+            messagingSenderId: '714200934043'
+        };
+        firebase.initializeApp(config);
+    }
 
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text> Hello </Text>
                 </View>
